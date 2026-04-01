9ac929c483c20ed530b1d507e9d03940a09e9c1b
jimin <jimmneu@snu.ac.kr>
2026-04-01 17:29:35 +0900

    modify func

--- main.cpp
+++ main.cpp
@@ -3,17 +3,28 @@
 
 using namespace std;
 
-int N,M;
+int N, M;
 vector<int> vec;
 
 void func(int cnt) {
+    if (cnt == M) {
+        for (int i = 0; i < M; i++) {
+            cout << vec[i];
+            if (i != M - 1) cout << ' ';
+        }
+        cout << '\n';
+        return;
+    }
 
+    int start;
+    if (vec.empty()) start = 1;
+    else start = vec.back() + 1;
 
-
-
-
-
-
+    for (int i = start; i <= N; i++) {
+        vec.push_back(i);
+        func(cnt + 1);
+        vec.pop_back();
+    }
 }
 
 int main() {
